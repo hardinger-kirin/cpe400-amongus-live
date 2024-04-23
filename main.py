8336e725e4417e42ed639025ac0b42360a230dc7
Kirin Hardinger <khardinger@nevada.unr.edu>
2024-04-23 12:27:45 -0700

    ID fix, Map, Display game code + player task list
    Empty or non-integer player IDs ignored
    Player IDs saved (different from Net IDs)
    Find game map
    Task list saved and displayed

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ if __name__ == "__main__":
 
     players_name = {}
     players_color = {}
+    players_id = {}
+    players_tasks = {}
+
+    current_map = -1
+
+    got_task_list = False
+    got_map = False
+    printed_tasks = False
 
     while True:
         capture = sniff(args.interface)
@@ -30,10 +38,17 @@ if __name__ == "__main__":
                         # keep track of players in game
                         if 'Name:' in str(field) and 'Scene Name' not in str(field):
                             if field.showname_value not in players_name:
+                                # empty values and non-integer IDs
+                                if not data[field_count - 3].showname_value.isnumeric() or not field.showname_value:
+                                    return
                                 players_name[data[field_count - 3].showname_value] = field.showname_value
 
                         if 'SetColor' in str(field) and field_count != len(data) - 1:
                             players_color[data[field_count - 1].showname_value] = data[field_count + 1].showname_value
+
+                    def get_ids():
+                        if 'Spawn Id' in str(field) and 'Player' in str(field.showname_value) and 'SetStartCounter' not in str(data):
+                            players_id[data[field_count + 8].showname_value] = data[field_count + 5].showname_value
 
                     def get_kills():
                         # provide murder data
@@ -64,7 +79,123 @@ if __name__ == "__main__":
                                 else:
                                     print(f'An unknown impostor of ID {murderer_id} murdered an unknown player of ID {victim_id}')
 
+                    def get_task_list():
+                        if 'SetTasks' in str(field):
+                            global got_task_list
+                            len_task_list = len(data) - field_count
+                            for i in range(field_count + 1, len_task_list, 3):
+                                if 'Part Length' in str(data[i].showname_key):
+                                    continue
+                                player_id = data[i].showname_value
+                                player_num_tasks = data[i + 1].showname_value
+                                player_tasks = data[i + 2].showname_value
+
+                                # prioritize displaying name over color
+                                if player_id in players_id:
+                                    players_tasks[players_name[players_id[player_id]]] = data[i + 2].showname_value
+                                elif player_id in players_color:
+                                    players_tasks[players_color[player_id]] = data[i + 2].showname_value
+                                else:
+                                    players_tasks[str("Unknown, ID: " + player_id)] = data[i + 2].showname_value
+
+                            got_task_list = True
+
+                    def get_map():
+                        if 'Spawn Id' in str(field) and 'ShipStatus' in str(field.showname_value):
+                            global got_map
+                            global current_map
+                            current_map = int(str(field.showname_value).replace(" (ShipStatus)", ''))
+                            got_map = True
+
+                    def print_tasks():
+                        global printed_tasks
+                        global current_map
+                        # https://stackoverflow.com/questions/13673060/split-string-into-strings-by-length
+                        for key in players_tasks:
+                            tasks = players_tasks[key]
+                            chunks, chunk_size = len(tasks), 2
+                            tasks = [tasks[i:i+chunk_size] for i in range(0, chunks, chunk_size)]
+                            print(f'Player {key} has tasks:',)
+                            if current_map == 0:
+                                for task in tasks:
+                                    print('\t', end='')
+                                    # https://stackoverflow.com/questions/209513/convert-hex-string-to-integer-in-python
+                                    task = int(str(task), 16)
+                                    if task == 0:
+                                        print(f'Admin: Swipe Card')
+                                    elif task == 1:
+                                        print(f'Electrical: Fix Wiring')
+                                    elif task == 2:
+                                        print(f'Weapons: Clear Asteroids')
+                                    elif task == 3:
+                                        print(f'Engines: Align Engine Output')
+                                    elif task == 4:
+                                        print(f'Medbay: Submit Scan')
+                                    elif task == 5:
+                                        print(f'Medbay: Inspect Sample')
+                                    elif task == 6:
+                                        print(f'Storage: Fuel Engines')
+                                    elif task == 7:
+                                        print(f'Reactor: Start Reactor')
+                                    elif task == 8:
+                                        print(f'O2: Empty Chute')
+                                    elif task == 9:
+                                        print(f'Cafeteria: Empty Garbage')
+                                    elif task == 10:
+                                        print(f'Communications: Download Data')
+                                    elif task == 11:
+                                        print(f'Electrical: Calibrate Distributor')
+                                    elif task == 12:
+                                        print(f'Navigation: Chart Course')
+                                    elif task == 13:
+                                        print(f'O2: Clean O2 Filter')
+                                    elif task == 14:
+                                        print(f'Reactor: Unlock Manifolds')
+                                    elif task == 15:
+                                        print(f'Electrical: Download Data')
+                                    elif task == 16:
+                                        print(f'Navigation: Stabilize Steering')
+                                    elif task == 17:
+                                        print(f'Weapons: Download Data')
+                                    elif task == 18:
+                                        print(f'Shields: Prime Shields')
+                                    elif task == 19:
+                                        print(f'Cafeteria: Download Data')
+                                    elif task == 20:
+                                        print(f'Navigation: Download Data')
+                                    elif task == 21:
+                                        print(f'Electrical: Divert Power to Shields')
+                                    elif task == 22:
+                                        print(f'Electrical: Divert Power to Weapons')
+                                    elif task == 23:
+                                        print(f'Electrical: Divert Power to Communications')
+                                    elif task == 24:
+                                        print(f'Electrical: Divert Power to Upper Engine')
+                                    elif task == 25:
+                                        print(f'Electrical: Divert Power to O2')
+                                    elif task == 26:
+                                        print(f'Electrical: Divert Power to Navigation')
+                                    elif task == 27:
+                                        print(f'Electrical: Divert Power to Lower Engine')
+                                    elif task == 28:
+                                        print(f'Electrical: Divert Power to Security')
+                            print('')
+                        printed_tasks = True
+
                     get_names_and_colors()
+                    get_ids()
+                    if not got_task_list:
+                        get_task_list()
+                    if not got_map:
+                        get_map()
+                    if got_task_list and got_map and not printed_tasks:
+                        print_tasks()
                     get_kills()
 
                     field_count += 1
+            elif int(packet.amongus.payload_type) == 1:
+                data = packet.amongus._get_all_fields_with_alternates()
+                field_count = 0
+                for field in data:
+                    if 'Game Code:' in str(field):
+                        print(f'Joined game. Code: {data[field_count + 4].showname_value}')

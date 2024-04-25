db30851d7aff294766385f37d2901a71eef93676
Kirin Hardinger <khardinger@nevada.unr.edu>
2024-04-25 11:04:44 -0700

    Implement tast list for other maps
    + case for unknown task ID

--- main.py
+++ main.py
@@ -183,6 +183,227 @@ if __name__ == "__main__":
                                         print(f'Electrical: Divert Power to Lower Engine')
                                     elif task == 28:
                                         print(f'Electrical: Divert Power to Security')
+                                    else:
+                                        print(f'[[Unknown Task]]')
+                            elif current_map == 1:
+                                for task in tasks:
+                                    print('\t', end='')
+                                    # https://stackoverflow.com/questions/209513/convert-hex-string-to-integer-in-python
+                                    task = int(str(task), 16)
+                                    if task == 0:
+                                        print(f'Hallway: Fix Wiring')
+                                    elif task == 1:
+                                        print(f'Admin: Enter ID Code')
+                                    elif task == 2:
+                                        print(f'Medbay: Submit Scan')
+                                    elif task == 3:
+                                        print(f'Balcony: Clear Asteroids')
+                                    elif task == 4:
+                                        print(f'Electrical: Divert Power to Admin')
+                                    elif task == 5:
+                                        print(f'Electrical: Divert Power to Cafeteria')
+                                    elif task == 6:
+                                        print(f'Electrical: Divert Power to Communications')
+                                    elif task == 7:
+                                        print(f'Electrical: Divert Power to Launchpad')
+                                    elif task == 8:
+                                        print(f'Electrical: Divert Power to Medbay')
+                                    elif task == 9:
+                                        print(f'Electrical: Divert Power to Office')
+                                    elif task == 10:
+                                        print(f'Storage: Water Plants')
+                                    elif task == 11:
+                                        print(f'Reactor: Start Reactor')
+                                    elif task == 12:
+                                        print(f'Electrical: Divert Power to Greenhouse')
+                                    elif task == 13:
+                                        print(f'Admin: Chart Course')
+                                    elif task == 14:
+                                        print(f'Greenhouse: Clean O2 Filter')
+                                    elif task == 15:
+                                        print(f'Launchpad: Fuel Engines')
+                                    elif task == 16:
+                                        print(f'Laboratory: Assemble Artifact')
+                                    elif task == 17:
+                                        print(f'Laboratory: Sort Samples')
+                                    elif task == 18:
+                                        print(f'Admin: Prime Shields')
+                                    elif task == 19:
+                                        print(f'Cafeteria: Empty Garbage')
+                                    elif task == 20:
+                                        print(f'Balcony: Measure Weather')
+                                    elif task == 21:
+                                        print(f'Electrical: Divert Power to Laboratory')
+                                    elif task == 22:
+                                        print(f'Cafeteria: Buy Beverage')
+                                    elif task == 23:
+                                        print(f'Office: Process Data')
+                                    elif task == 24:
+                                        print(f'Launchpad: Run Diagnostics')
+                                    elif task == 25:
+                                        print(f'Reactor: Unlock Manifolds')
+                                    else:
+                                        print(f'[[Unknown Task]]')
+                            elif current_map == 2:
+                                for task in tasks:
+                                    print('\t', end='')
+                                    # https://stackoverflow.com/questions/209513/convert-hex-string-to-integer-in-python
+                                    task = int(str(task), 16)
+                                    if task == 0:
+                                        print(f'Office: Swipe Card')
+                                    elif task == 1:
+                                        print(f'Dropship: Insert Keys')
+                                    elif task == 2:
+                                        print(f'Office: Scan Boarding Pass')
+                                    elif task == 3:
+                                        print(f'Electrical: Fix Wiring')
+                                    elif task == 4:
+                                        print(f'Weapons: Download Data')
+                                    elif task == 5:
+                                        print(f'Office: Download Data')
+                                    elif task == 6:
+                                        print(f'Electrical: Download Data')
+                                    elif task == 7:
+                                        print(f'Specimen Room: Download Data')
+                                    elif task == 8:
+                                        print(f'Storage: Download Data')
+                                    elif task == 9:
+                                        print(f'Specimen Room: Start Reactor')
+                                    elif task == 10:
+                                        print(f'Storage: Fuel Engines')
+                                    elif task == 11:
+                                        print(f'Boiler Room: Open Waterways')
+                                    elif task == 12:
+                                        print(f'Medbay: Inspect Sample')
+                                    elif task == 13:
+                                        print(f'Boiler Room: Replace Water Jug')
+                                    elif task == 14:
+                                        print(f'Outside: Fix Weather Node Node_GI')
+                                    elif task == 15:
+                                        print(f'Outside: Fix Weather Node Node_IRO')
+                                    elif task == 16:
+                                        print(f'Outside: Fix Weather Node Node_PD')
+                                    elif task == 17:
+                                        print(f'Outside: Fix Weather Node Node_TB')
+                                    elif task == 18:
+                                        print(f'Communications: Reboot WiFi')
+                                    elif task == 19:
+                                        print(f'O2: Monitor Tree')
+                                    elif task == 20:
+                                        print(f'Specimen Room: Unlock Manifolds')
+                                    elif task == 21:
+                                        print(f'Specimen Room: Store Artifacts')
+                                    elif task == 22:
+                                        print(f'O2: Fill Canisters')
+                                    elif task == 23:
+                                        print(f'O2: Empty Garbage')
+                                    elif task == 24:
+                                        print(f'Dropship: Chart Course')
+                                    elif task == 25:
+                                        print(f'Medbay: Submit Scan')
+                                    elif task == 26:
+                                        print(f'Weapons: Clear Asteroids')
+                                    elif task == 27:
+                                        print(f'Outside: Fix Weather Node Node_CA')
+                                    elif task == 28:
+                                        print(f'Outside: Fix Weather Node Node_MLG')
+                                    elif task == 29:
+                                        print(f'Laboratory: Align Telescope')
+                                    elif task == 30:
+                                        print(f'Laboratory: Repair Drill')
+                                    elif task == 31:
+                                        print(f'Laboratory: Record Temperature')
+                                    elif task == 32:
+                                        print(f'Outside: Record Temperature')
+                                    else:
+                                        print(f'[[Unknown Task]]')
+                            elif current_map == 3:
+                                for task in tasks:
+                                    print('\t', end='')
+                                    # https://stackoverflow.com/questions/209513/convert-hex-string-to-integer-in-python
+                                    task = int(str(task), 16)
+                                    if task == 0:
+                                        print(f'Electrical: Fix Wiring')
+                                    elif task == 1:
+                                        print(f'Meeting Room: Enter ID Code')
+                                    elif task == 2:
+                                        print(f'Electrical: Reset Breakers')
+                                    elif task == 3:
+                                        print(f'Vault Room: Download Data')
+                                    elif task == 4:
+                                        print(f'Brig: Download Data')
+                                    elif task == 5:
+                                        print(f'Cargo Bay: Download Data')
+                                    elif task == 6:
+                                        print(f'Gap Room: Download Data')
+                                    elif task == 7:
+                                        print(f'Records: Download Data')
+                                    elif task == 8:
+                                        print(f'Cargo Bay: Unlcok Safe')
+                                    elif task == 9:
+                                        print(f'Ventilation: Start Fans')
+                                    elif task == 10:
+                                        print(f'Main Hall: Empty Garbage')
+                                    elif task == 11:
+                                        print(f'Medical: Empty Garbage')
+                                    elif task == 12:
+                                        print(f'Kitchen: Empty Garbage')
+                                    elif task == 13:
+                                        print(f'Main Hall: Develop Photos')
+                                    elif task == 14:
+                                        print(f'Cargo Bay: Fuel Engines')
+                                    elif task == 15:
+                                        print(f'Security: Rewind Tapes')
+                                    elif task == 16:
+                                        print(f'Vault Room: Polish Ruby')
+                                    elif task == 17:
+                                        print(f'Electrical: Calibrate Distributor')
+                                    elif task == 18:
+                                        print(f'Cockpit: Stabilize Steering')
+                                    elif task == 19:
+                                        print(f'Armory: Download Data')
+                                    elif task == 20:
+                                        print(f'Cockpit: Download Data')
+                                    elif task == 21:
+                                        print(f'Comms: Download Data')
+                                    elif task == 22:
+                                        print(f'Medical: Download Data')
+                                    elif task == 23:
+                                        print(f'Viewing Deck: Download Data')
+                                    elif task == 24:
+                                        print(f'Electrical: Divert Power to Armory')
+                                    elif task == 25:
+                                        print(f'Electrical: Divert Power to Cockpit')
+                                    elif task == 26:
+                                        print(f'Electrical: Divert Power to Gap Room')
+                                    elif task == 27:
+                                        print(f'Electrical: Divert Power to Main Hall')
+                                    elif task == 28:
+                                        print(f'Electrical: Divert Power to Meeting Room')
+                                    elif task == 29:
+                                        print(f'Electrical: Divert Power to Showers')
+                                    elif task == 30:
+                                        print(f'Electrical: Divert Power to Engine')
+                                    elif task == 31:
+                                        print(f'Showers: Pick Up Towels')
+                                    elif task == 32:
+                                        print(f'Lounge: Clean Toilet')
+                                    elif task == 33:
+                                        print(f'Vault Room: Dress Mannequin')
+                                    elif task == 34:
+                                        print(f'Records: Sort Records')
+                                    elif task == 35:
+                                        print(f'Armory: Put Away Pistols')
+                                    elif task == 36:
+                                        print(f'Armory: Put Away Rifles')
+                                    elif task == 37:
+                                        print(f'Main Hall: Decontaminate')
+                                    elif task == 38:
+                                        print(f'Kitchen: Make Burger')
+                                    elif task == 39:
+                                        print(f'Showers: Fix Shower')
+                                    else:
+                                        print(f'[[Unknown Task]]')
                             print('')
                         printed_tasks = True
 
